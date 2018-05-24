a7bdbac12fa543069d6b5a69e84a4089b82740bd
Capblancq <thibaut.capblancq@gmail.com>
2018-05-24 14:48:57 +0200

    Update Script_RDA.R

--- Script_RDA.R
+++ Script_RDA.R
@@ -27,8 +27,8 @@ library(factoextra)
 
 rdadapt<-function(rda,K)
 {
-  zscores<-rda$CCA$v[,1:as.numeric(K)]
-  resscale <- apply(zscores, 2, scale)
+  loadings<-rda$CCA$v[,1:as.numeric(K)]
+  resscale <- apply(loadings, 2, scale)
   resmaha <- covRob(resscale, distance = TRUE, na.action= na.omit, estim="pairwiseGK")$dist
   lambda <- median(resmaha)/qchisq(0.5,df=K)
   reschi2test <- pchisq(resmaha/lambda,K,lower.tail=FALSE)
